41471d59859fee8e76c9363b21dc8c8fd48bc664
jonibek99 <jonibekannaev5@gmail.com>
2025-04-24 21:14:26 +0500

    my4

--- arithmetic_op04.py
+++ arithmetic_op04.py
@@ -9,3 +9,9 @@
 #Create a variable called 'answer' and assign it the sum of the three digits x1, x2, x3.
 
 #Print the value of the 'answer.
+number=432
+x1=int(str(number)[1])
+x2=int(str(number)[2])
+x3=int(str(number)[-1])
+answer=x1+x2+x3
+print(answer)
